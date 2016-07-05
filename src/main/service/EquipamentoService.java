66b61bc12ad8cddc2290f5cfd41953c75e415847
Rodrigo Freitas <rodrigo.freitas@tecsinapse.com.br>
2016-07-05 20:57:15 -0300

    initial exemple

--- src/main/service/EquipamentoService.java
+++ src/main/service/EquipamentoService.java
@@ -3,6 +3,8 @@ package main.service;
 import main.service.Conexao;
 import main.model.Equipamento;
 
+import java.sql.SQLException;
+
 
 public class EquipamentoService extends Conexao {
 
@@ -10,29 +12,34 @@ public class EquipamentoService extends Conexao {
         super();
     }
 
-/*    public int insert(Equipamento equipamento){
+    public int insert(Equipamento equipamento){
         int cont =1;
         this.setPreparedStatement("INSERT INTO equipamento (patrimonio, nome, descricao, local_uso, marca) VALUES (?,?,?,?,?)");
-        this.getPreparedStatement().setLong(cont++, equipamento.getNome());
-        this.getPreparedStatement().setString(cont++, especieBO.getNomeCientifico());
-        this.getPreparedStatement().setInt(cont++, especieBO.getStatus());
+        this.getPreparedStatement().setLong(cont++, equipamento.getPatrimonio());
+        this.getPreparedStatement().setString(cont++, equipamento.getNome());
+        this.getPreparedStatement().setString(cont++, equipamento.getDescricao());
+        this.getPreparedStatement().setString(cont++, equipamento.getLocalDeUso());
+        this.getPreparedStatement().setString(cont++, equipamento.getMarca());
         int retorno = this.getPreparedStatement().executeUpdate();
         this.getPreparedStatement().close();
         return retorno;
     }
 
-    public EspecieBO findById(int pk) throws SQLException {
-        EspecieBO especieBO = new EspecieBO();
+    public Equipamento findById(int pk) throws SQLException {
+        Equipamento equipamento = new Equipamento();
         this.setPreparedStatement(" SELECT id, patrimonio, nome, descricao, local_uso, marca  FROM equipamento WHERE id = ?");
-        this.getPreparedStatement().setInt(1, idEspecia);
+        this.getPreparedStatement().setInt(1, pk);
         this.setResultSet(getPreparedStatement().executeQuery());
         while (this.getResultSet().next()) {
-            especieBO.setIdEspecie(getResultSet().getInt("id_especie"));
-            especieBO.setNomeSimplificado(getResultSet().getString("nome_simplificado"));
-            especieBO.setNomeCientifico(getResultSet().getString("nome_cientifico"));
+            equipamento.setId(getResultSet().getLong("id"));
+            equipamento.setPatrimonio(getResultSet().getLong("patrimonio"));
+            equipamento.setNome(getResultSet().getString("nome"));
+            equipamento.setDescricao(getResultSet().getString("descricao"));
+            equipamento.setLocalDeUso(getResultSet().getString("local_uso"));
+            equipamento.setMarca(getResultSet().getString("marca"));
         }
         this.getPreparedStatement().close();
-        return especieBO;
-    }*/
+        return equipamento;
+    }
 
 }
